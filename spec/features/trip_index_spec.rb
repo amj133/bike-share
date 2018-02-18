87b46abe7813f533040bc5758c69225a14a34b7f
Andrew Dymond <andy.dymond@gmail.com>
2018-02-18 16:13:30 -0700

    Add next page and previous page links to index view, visitor sees next 30 trips spec passing

--- spec/features/trip_index_spec.rb
+++ spec/features/trip_index_spec.rb
@@ -1,13 +1,12 @@
 require "rails_helper"
 
 describe "visitor sees trips index" do
-  it "displays 30 trips per page" do
+  before(:context) do
     create(:station)
     create(:station)
-    31.times do
-      create(:trip)
-    end
-
+    create_list(:trip, 61)
+  end
+  it "displays first 30 trips per page" do
     visit trips_path
 
     expect(page).to have_content("Displaying Trips 1-30")
@@ -19,26 +18,22 @@ describe "visitor sees trips index" do
     expect(page).to have_content(Trip.first.bike_id)
     expect(page).to have_content(Trip.first.subscription)
     expect(page).to have_content(Trip.first.zipcode)
-    expect(page).to have_content(Trip.all[-2].duration)
+    expect(page).to have_content(Trip.all[29].duration)
     expect(page).to_not have_content(Trip.last.duration)
     expect(page).to have_link("Next Page")
   end
   it "displays next 30 trips to visitor" do
-    create(:station)
-    create(:station)
-    61.times do
-      create(:trip)
-    end
-
     visit trips_path
+
     click_on "Next Page"
 
     expect(page).to have_content("Displaying Trips 31-60")
     expect(page).to have_content(Trip.all[30].duration)
     expect(page).to have_content(Trip.all[59].duration)
+    expect(page).to_not have_content(Trip.all[29].duration)
 
     click_on "Previous Page"
-    
+
     have_content(Trip.first.duration)
     expect(page).to have_content(Trip.all[29].duration)
   end
