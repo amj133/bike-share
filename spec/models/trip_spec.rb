47721e6d0f3d5df6b95ec49c9cad5845ef3dbf3b
Andrew Dymond <andy.dymond@gmail.com>
2018-02-18 13:53:17 -0700

    Add relationship tests to station_spec & trip_spec

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 describe Trip, type: :model do
   describe "validations" do
-    it { should validate_presence_of(:duration) }
-    it { should validate_presence_of(:start_date) }
-    it { should validate_presence_of(:start_station_id) }
-    it { should validate_presence_of(:end_date) }
-    it { should validate_presence_of(:end_station_id) }
-    it { should validate_presence_of(:bike_id) }
-    it { should validate_presence_of(:subscription) }
-    it { should validate_presence_of(:zipcode) }
+    it {should validate_presence_of(:duration)}
+    it {should validate_presence_of(:start_date)}
+    it {should validate_presence_of(:start_station_id)}
+    it {should validate_presence_of(:end_date)}
+    it {should validate_presence_of(:end_station_id)}
+    it {should validate_presence_of(:bike_id)}
+    it {should validate_presence_of(:subscription)}
+    it {should validate_presence_of(:zipcode)}
+  end
+  describe "relationships" do
+    it {should belong_to :start_station}
+    it {should belong_to :end_station}
   end
 end
