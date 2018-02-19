9cf89c7c696c4a939e922320d4343a82e1caeceb
kylesallette <kylesallette@gmail.com>
2018-02-19 10:47:44 -0700

    user model test..not yet passing

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+  describe User, type: :model do
+    describe "validations" do
+      it {should validate_presence_of(:username)}
+      it {should validate_uniqueness_of(:username)}
+    end
+  end 
+
+    describe "roles" do
+      it "can be created as admin" do
+        kyle = User.create!(username: "kyle",
+                             password: "password",
+                             role:  1)
+
+          expect(kyle.role).to eq("admin")
+          expect(bob.admin?).to be_truthy
+      end
+    end
+
+      describe "roles" do
+        it "can be created as registered" do
+          bob = User.create!(username: "bob",
+                              password: "password",
+                              role:  0)
+
+          expect(bob.role).to eq("default")
+          expect(bob.admin?).to_not be_truthy
+
+
+    end
+  end
