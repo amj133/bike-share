c5ec1eb6729abc9434b0267e332b1a8a42188ee1
kylesallette <kylesallette@gmail.com>
2018-02-19 13:03:37 -0700

    adding dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
 
+  root to: "welcome#index"
+
   resources :trips, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :stations, only: [:index, :show]
+
+  resources :users, only: [:show, :new, :create]
+  get "/dashboard", to: "users#show"
+
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
