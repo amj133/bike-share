abf508e3552dfceb56fb34d9fe935267affca128
amj133 <amj@vt.edu>
2018-02-19 08:03:08 -0700

    create visitor goes to station show - test passing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :trips, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :stations, only: [:index]
+  resources :stations, only: [:index, :show]
 end
