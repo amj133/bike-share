c264e3754270c2042ffc64658c5e0d695522dc8d
Andrew Dymond <andy.dymond@gmail.com>
2018-02-19 15:43:53 -0700

    Add avg_ride_duration class method to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,52 @@ class Trip < ApplicationRecord
                         :end_station_id, :bike_id, :subscription, :zipcode
   belongs_to :start_station, class_name: 'Station', foreign_key: 'start_station_id'
   belongs_to :end_station, class_name: 'Station', foreign_key: 'end_station_id'
+
+  def self.avg_ride_duration
+    average(:duration)
+  end
+
+  # def self.longest_ride
+  #
+  # end
+  #
+  # def self.shortest_ride
+  #
+  # end
+  #
+  # def most_starting_station
+  #
+  # end
+  #
+  # def most_ending_station
+  #
+  # end
+  #
+  # def month_breakdown
+  #
+  # end
+  #
+  # def self.most_ridden_bike
+  #
+  # end
+  #
+  # def self.least_ridden_bike
+  #
+  # end
+  #
+  # def self.user_subscrip_count
+  #
+  # end
+  #
+  # def self.user_subscrip_percentage
+  #
+  # end
+  #
+  # def self.most_popular_date
+  #
+  # end
+  #
+  # def self.least_popular_date
+  #
+  # end
 end
