e6fda53fd3e439cf0b780f573b4e5b9f2b08b763
amj133 <amj@vt.edu>
2018-02-19 08:06:03 -0700

    changed station factor so lat, long, dock_count are integers and increment properly

--- spec/factories/stations.rb
+++ spec/factories/stations.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :station do
     sequence(:name) {|n| "Name#{n}"}
-    sequence(:lat) {|n| "lat#{n}"}
-    sequence(:long) {|n| "long#{n}"}
-    sequence(:dock_count) {|n| "dock_count#{n}"}
+    sequence(:lat) {|n| n}
+    sequence(:long) {|n| n}
+    sequence(:dock_count) {|n| n}
     sequence(:city) {|n| "City#{n}"}
     installation_date(DateTime.now)
   end
