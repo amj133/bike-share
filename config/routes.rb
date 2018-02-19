d11b4b3bd2cd8954941fd594144780dedec6eab0
Andrew Dymond <andy.dymond@gmail.com>
2018-02-19 12:10:17 -0700

    Add show action to conditions controller & display condition data in show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   resources :trips, only: [:index]
-  resources :conditions, only: [:index]
+  resources :conditions, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
