08ff64d5fb591baadf201ff15966c59e1181a71c
amj133 <amj@vt.edu>
2018-02-19 07:53:39 -0700

    visitor sees station index spec created and passing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   resources :trips, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :stations, only: [:index]
 end
