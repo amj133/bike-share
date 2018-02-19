b58356fc7a1f8b4d9e8a841d5287b2b9a100c3ec
Andrew Dymond <andy.dymond@gmail.com>
2018-02-19 12:04:58 -0700

    Add condition_show_spec

---
+++ spec/features/condition_show_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "user sees one condition" do
+  it "displays" do
+    condition = create(:condition)
+
+    visit condition_path(condition)
+
+    expect(page).to have_content(condition.date)
+    expect(page).to have_content(condition.max_temp_f)
+    expect(page).to have_content(condition.mean_temp_f)
+    expect(page).to have_content(condition.min_temp_f)
+    expect(page).to have_content(condition.mean_humidity)
+    expect(page).to have_content(condition.mean_visibility_miles)
+    expect(page).to have_content(condition.mean_wind_speed)
+    expect(page).to have_content(condition.precipitation_inches)
+  end
+end
