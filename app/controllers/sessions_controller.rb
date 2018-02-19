21021f98c5e7a450fcb9d2f98a411aac54001e97
amj133 <amj@vt.edu>
2018-02-19 13:53:29 -0700

    user can logout added to user can login feature test - all tests passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,22 @@
 class SessionsController < ApplicationController
 
   def new
+  end
 
-  end 
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    session.destroy
+
+    redirect_to root_path
+  end
 
 end
