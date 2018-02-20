a1a1090f3a077cffb3472cc37f332c259b7add66
amj133 <amj@vt.edu>
2018-02-20 13:36:39 -0700

    updated seed file to conver to proper data types

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,59 +9,63 @@ require 'csv'
 
 stations = CSV.open('db/csv/station.csv', headers: true, header_converters: :symbol)
 stations.each do |row|
+  date_split = row[:installation_date].split("/")
   Station.create(name: row[:name],
-                 lat: row[:lat],
-                 long: row[:long],
-                 dock_count: row[:dock_count],
+                 lat: row[:lat].to_f,
+                 long: row[:long].to_f,
+                 dock_count: row[:dock_count].to_i,
                  city: row[:city],
-                 installation_date: row[:installation_date])
+                 installation_date: DateTime.new(date_split[2].to_i, date_split[0].to_i, date_split[1].to_i))
 end
 
 statuses = CSV.open('db/csv/status.csv', headers: true, header_converters: :symbol)
 statuses.each do |row|
-  Status.create(station_id: row[:station_id],
-                bikes_available: row[:bikes_available],
-                docks_available: row[:docks_available],
-                time: row[:time])
+  date_time_split = row[:time].split(/[\/: ]/)
+  Status.create(station_id: row[:station_id].to_i,
+                bikes_available: row[:bikes_available].to_i,
+                docks_available: row[:docks_available].to_i,
+                time: DateTime.new(date_time_split[0].to_i, date_time_split[1].to_i, date_time_split[2].to_i, date_time_split[3].to_i, date_time_split[4].to_i, date_time_split[5].to_i))
 end
 
 trips = CSV.open('db/csv/trip.csv', headers: true, header_converters: :symbol)
 trips.each do |row|
-  Trip.create(duration: row[:duration],
-              start_date: row[:start_date],
-              start_station_id: row[:start_station_id],
-            end_date: row[:end_date],
-          end_station_id: row[:end_station_id],
-              bike_id: row[:bike_id],
-              subscription: row[:subscription_type],
-              zipcode: row[:zip_code])
+  start_date_split = row[:start_date].split[0].split("/")
+  end_date_split = row[:end_date].split[0].split("/")
+  Trip.create(duration: row[:duration].to_i,
+              start_date: DateTime.new(start_date_split[2].to_i, start_date_split[0].to_i, start_date_split[1].to_i),
+              start_station_id: row[:start_station_id].to_i,
+              end_date: DateTime.new(end_date_split[2].to_i, end_date_split[0].to_i, end_date_split[1].to_i),
+              end_station_id: row[:end_station_id].to_i,
+              bike_id: row[:bike_id].to_i,
+              subscription: row[:subscription_type])
+              # zipcode: row[:zip_code].to_i)
 end
 
-conditions = CSV.open('db/csv
-   weather.csv', headers: true, header_converters: :symbol)
+conditions = CSV.open('db/csv/weather.csv', headers: true, header_converters: :symbol)
 conditions.each do |row|
-  Condition.create(date: row[:date],
-                   max_temp_f: row[:max_temperature_f],
-                   mean_temp_f: row[:mean_temperature_f],
-                   min_temp_f: row[:min_temperature_f],
-                   max_dewpoint_f: row[:max_dew_point_f],
-                   mean_dewpoint_f: row[:mean_dew_point_f],
-                   min_dewpoint_f: row[:min_dew_point_f],
-                   max_humidity: row[:max_humidity],
-                   mean_humidity: row[:mean_humidity],
-                   min_humidity: row[:min_humidity],
-                   max_sea_level_pressure_inches: row[:max_sea_level_pressure_inches],
-                   mean_sea_level_pressure_inches: row[:mean_sea_level_pressure_inches],
-                   min_sea_level_pressure_inches: row[:min_sea_level_pressure_inches],
-                   max_visibility_miles: row[:max_visibility_miles],
-                   mean_visibility_miles: row[:mean_visibility_miles],
-                   min_visibility_miles: row[:min_visibility_miles],
-                   max_wind_speed: row[:max_wind_speed_mph],
-                   mean_wind_speed: row[:mean_wind_speed_mph],
-                   max_gust_speed: row[:max_gust_speed_mph],
-                   precipitation_inches: row[:precipitation_inches],
-                   cloud_cover: row[:cloud_cover],
-                   events: row[:events],
-                   wind_dir_degrees: row[:wind_dir_degrees],
-                   zipcode: row[:zip_code])
+  date_split = row[:date].split("/")
+  Condition.create(date: DateTime.new(date_split[2].to_i, date_split[0].to_i, date_split[1].to_i),
+                   max_temp_f: row[:max_temperature_f].to_f,
+                   mean_temp_f: row[:mean_temperature_f].to_f,
+                   min_temp_f: row[:min_temperature_f].to_f,
+                   max_dewpoint_f: row[:max_dew_point_f].to_f,
+                   mean_dewpoint_f: row[:mean_dew_point_f].to_f,
+                   min_dewpoint_f: row[:min_dew_point_f].to_f,
+                   max_humidity: row[:max_humidity].to_f,
+                   mean_humidity: row[:mean_humidity].to_f,
+                   min_humidity: row[:min_humidity].to_f,
+                   max_sea_level_pressure_inches: row[:max_sea_level_pressure_inches].to_f,
+                   mean_sea_level_pressure_inches: row[:mean_sea_level_pressure_inches].to_f,
+                   min_sea_level_pressure_inches: row[:min_sea_level_pressure_inches].to_f,
+                   max_visibility_miles: row[:max_visibility_miles].to_f,
+                   mean_visibility_miles: row[:mean_visibility_miles].to_f,
+                   min_visibility_miles: row[:min_visibility_miles].to_f,
+                   max_wind_speed: row[:max_wind_speed_mph].to_f,
+                   mean_wind_speed: row[:mean_wind_speed_mph].to_f,
+                   max_gust_speed: row[:max_gust_speed_mph].to_f,
+                   precipitation_inches: row[:precipitation_inches].to_f,
+                   cloud_cover: row[:cloud_cover].to_f,
+                   events: row[:events].to_f,
+                   wind_dir_degrees: row[:wind_dir_degrees].to_f,
+                   zipcode: row[:zip_code].to_i)
 end
