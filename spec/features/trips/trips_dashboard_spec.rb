4a4841dcace66162ca58d41ed0abfb066ede9fd2
Andrew Dymond <andy.dymond@gmail.com>
2018-02-20 15:26:10 -0700

    Add all trip analytics expectations to features/trip_dashboard_spec

--- spec/features/trips/trips_dashboard_spec.rb
+++ spec/features/trips/trips_dashboard_spec.rb
@@ -9,16 +9,16 @@ describe "user visits trip dashboard" do
     visit trips_dashboard_path
 
     expect(page).to have_content(Trip.avg_ride_duration)
-    # expect(page).to have_content(Trip.longest_ride)
-    # expect(page).to have_content(Trip.shortest_ride)
-    # expect(page).to have_content(Trip.most_starting_station)
-    # expect(page).to have_content(Trip.most_ending_station)
-    # expect(page).to have_content(Trip.month_breakdown)
-    # expect(page).to have_content(Trip.most_ridden_bike)
-    # expect(page).to have_content(Trip.least_ridden_bike)
-    # expect(page).to have_content(Trip.user_subscrip_count)
-    # expect(page).to have_content(Trip.user_subscrip_percentage)
-    # expect(page).to have_content(Trip.most_popular_date)
-    # expect(page).to have_content(Trip.least_popular_date)
+    expect(page).to have_content(Trip.longest_ride)
+    expect(page).to have_content(Trip.shortest_ride)
+    expect(page).to have_content(Trip.most_starting_station)
+    expect(page).to have_content(Trip.most_ending_station)
+    expect(page).to have_content(Trip.month_breakdown)
+    expect(page).to have_content(Trip.most_ridden_bike)
+    expect(page).to have_content(Trip.least_ridden_bike)
+    expect(page).to have_content(Trip.user_subscrip_count)
+    expect(page).to have_content(Trip.user_subscrip_percentage)
+    expect(page).to have_content(Trip.most_popular_date)
+    expect(page).to have_content(Trip.least_popular_date)
   end
 end
