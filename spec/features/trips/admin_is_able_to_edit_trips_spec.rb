daed3ebed5bba5c89fb0a731a4bd12d4f1c9c004
kylesallette <kylesallette@gmail.com>
2018-02-20 12:27:32 -0700

    test for admin redirected to edit page passing

---
+++ spec/features/trips/admin_is_able_to_edit_trips_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+
+  describe "Admin sees trip index"  do
+    it "Admin sees edit button next to trip"  do
+      station = Station.create!(name: "Big Bikes", lat: 123456, long:223523, dock_count:22, city:"denver" ,installation_date: Time.now)
+      trip = Trip.create!(duration: 73, start_date: Time.now, start_station_id: 1,
+                          end_date: Time.now, end_station_id: 1, bike_id: 22, subscription: "Subscriber",
+                          zipcode: 32218)
+      user = User.create(username: "Kyle", email: "email",
+                          password: "password" ,role: 1)
+
+        visit root_path
+
+        click_link("Login")
+
+        fill_in("Username", with: "Kyle")
+        fill_in("Password", with: "password")
+
+        click_button("Log in")
+
+        visit trips_path
+
+       expect(page).to have_link("Edit")
+    end
+  end
+
+  describe "Admin sees trip index"  do
+    it "Admin clicks edit and sees edit page"  do
+      station = Station.create!(name: "Big Bikes", lat: 123456, long:223523, dock_count:22, city:"denver" ,installation_date: Time.now)
+      trip = Trip.create!(duration: 73, start_date: Time.now, start_station_id: 1,
+                          end_date: Time.now, end_station_id: 1, bike_id: 22, subscription: "Subscriber",
+                          zipcode: 32218)
+      user = User.create(username: "Kyle", email: "email",
+                          password: "password" ,role: 1)
+
+        visit root_path
+
+        click_link("Login")
+
+        fill_in("Username", with: "Kyle")
+        fill_in("Password", with: "password")
+
+        click_button("Log in")
+
+        visit trips_path
+
+        click_link("Edit")
+
+        expect(page).to have_content("Edit Trips")
+    end
+  end
