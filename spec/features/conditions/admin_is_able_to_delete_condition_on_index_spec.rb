c5712facd76a9a489cffa3797056f45904ee4fbc
kylesallette <kylesallette@gmail.com>
2018-02-19 23:08:18 -0700

    adding admin delelet station test

---
+++ spec/features/conditions/admin_is_able_to_delete_condition_on_index_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+  describe "Admin sees conditions index"  do
+    it "Admin sees delete button next to conditions"  do
+        condition = create(:condition)
+        user = User.create(username: "Kyle", email: "email",
+        password: "password" ,role: 1)
+
+        visit root_path
+
+        click_link("Login")
+
+        fill_in("Username", with: "Kyle")
+        fill_in("Password", with: "password")
+
+        click_button("Log in")
+
+        visit conditions_path
+
+
+        expect(page).to have_content(condition.date)
+        expect(page).to have_link("Delete")
+
+
+    end
+  end
+  describe "user sees conditions index"  do
+    it "User does not see delete button"  do
+        condition = create(:condition)
+        user = User.create(username: "Kyle", email: "email",
+        password: "password")
+
+        visit root_path
+
+        click_link("Login")
+
+        fill_in("Username", with: "Kyle")
+        fill_in("Password", with: "password")
+
+        click_button("Log in")
+
+        visit conditions_path
+
+
+        expect(page).to have_content(condition.date)
+        expect(page).to_not have_link("Deleted")
+      end
+    end
+
+    describe "Admin see conditions index"  do
+      it "Admin is able to delete a condition"  do
+        condition = create(:condition)
+        user = User.create(username: "Kyle", email: "email",
+        password: "password", role: 1)
+
+        visit root_path
+
+        click_link("Login")
+
+        fill_in("Username", with: "Kyle")
+        fill_in("Password", with: "password")
+
+        click_button("Log in")
+
+        visit conditions_path
+
+        expect(page).to have_content(condition.date)
+
+        click_link("Delete")
+
+        expect(page).to_not have_content(condition.date)
+        expect(page).to have_content("Condition deleted.")
+      end
+    end
