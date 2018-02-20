54d5ce5327b8832351a677f96ad164410a5a90f2
kylesallette <kylesallette@gmail.com>
2018-02-20 12:26:34 -0700

    edit and update for admin trips controller

--- app/controllers/admin/trips_controller.rb
+++ app/controllers/admin/trips_controller.rb
@@ -1,5 +1,10 @@
 class Admin::TripsController < Admin::BaseController
 
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
   def destroy
     @trip = Trip.find(params[:id])
     @trip.destroy
@@ -8,7 +13,21 @@ class Admin::TripsController < Admin::BaseController
   end
 
 
+  def update
+   @trip = Trip.find(params[:id])
+   @trip.update(trip_params)
+    if @trip.save
+      redirect_to admin_trips_path
+    else
+     render :edit
+    end
+  end
 
+  private
 
+  def trip_params
+    params.require(:trip).permit(:duration, :start_date, :start_station_id, :end_date,
+                                :end_station_id, :bike_id, :subscription, :zipcode )
+  end
 
 end
