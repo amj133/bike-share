eb96bc749faad9ecc1560a0a1646860c5cd1ed45
kylesallette <kylesallette@gmail.com>
2018-02-20 12:44:01 -0700

    adding test that trip is updated passing

--- spec/features/trips/admin_is_able_to_edit_trips_spec.rb
+++ spec/features/trips/admin_is_able_to_edit_trips_spec.rb
@@ -48,5 +48,40 @@ require 'rails_helper'
         click_link("Edit")
 
         expect(page).to have_content("Edit Trips")
+        expect(page).to have_content("Duration")
+        expect(page).to have_content("Start station")
+        expect(page).to have_content("Zipcode")
+    end
+  end
+
+  describe "Admin sees trip index"  do
+    it "Admin clicks edit and sees edit page and is able to edit info"  do
+      station = Station.create!(name: "Big Bikes", lat: 123456, long:223523, dock_count:22, city:"denver" ,installation_date: Time.now)
+      trip = Trip.create!(duration: 73, start_date: Time.now, start_station_id: 1,
+                          end_date: Time.now, end_station_id: 1, bike_id: 22, subscription: "Subscriber",
+                          zipcode: 32218)
+      user = User.create(username: "Kyle", email: "email",
+                          password: "password" ,role: 1)
+
+        visit root_path
+
+        click_link("Login")
+
+        fill_in("Username", with: "Kyle")
+        fill_in("Password", with: "password")
+
+        click_button("Log in")
+
+        visit trips_path
+
+        click_link("Edit")
+
+        expect(page).to have_content("Edit Trips")
+
+        fill_in('Duration', with: '666')
+
+        click_button("Update Trip")
+
+        expect(page).to have_content(666)
     end
   end
