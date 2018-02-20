2a88758886b92815cf11865fbc787b72682d3b3b
kylesallette <kylesallette@gmail.com>
2018-02-19 20:07:10 -0700

    adding destroy

--- app/controllers/conditions_controller.rb
+++ app/controllers/conditions_controller.rb
@@ -6,4 +6,12 @@ class ConditionsController < ApplicationController
  def show
    @condition = Condition.find(params[:id])
  end
+
+ def destroy
+   @condition = Condition.find(params[:id])
+   @condition.destroy
+   flash.notice = "Condition deleted."
+   redirect_to conditions_path
+ end
+
 end
