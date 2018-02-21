49beb0fa98c5ad4a69dddd55e97ecdbf4c26a6f6
amj133 <amj@vt.edu>
2018-02-21 15:38:24 -0700

    wrote visitor can add accessories to cart spec

---
+++ spec/features/cart/visitor_can_add_accessories_to_cart_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "visitor can add accessories to cart" do
+  describe "goes to bike-shop path" do
+    it "allows visitor to add accessory to cart" do
+      helmet = Accessory.create!(name: "helmet",
+                                 description: "awesome blue helmet",
+                                 price: 3450)
+
+      visit accessories_path
+      click_on("Add to cart")
+
+      expect(page).to have_content("You now have 1 #{helmet.name} in your cart.")
+
+      visit conditions_path
+
+      expect(page).to have_content("1 item in cart")
+    end
+  end
+end
