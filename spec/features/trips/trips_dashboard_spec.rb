7da42129c2687d1dafee9cc402b54233313c57c8
Andrew Dymond <andy.dymond@gmail.com>
2018-02-20 19:33:27 -0700

    Add displays weather stats for longest and shortest rides to trips_dashboard_spec

--- spec/features/trips/trips_dashboard_spec.rb
+++ spec/features/trips/trips_dashboard_spec.rb
@@ -11,8 +11,8 @@ describe "user visits trip dashboard" do
     visit trips_dashboard_path
 
     expect(page).to have_content(Trip.avg_ride_duration)
-    expect(page).to have_content(Trip.longest_ride)
-    expect(page).to have_content(Trip.shortest_ride)
+    expect(page).to have_content(Trip.longest_ride.duration)
+    expect(page).to have_content(Trip.shortest_ride.duration)
     expect(page).to have_content(Trip.most_starting_station.name)
     expect(page).to have_content(Trip.most_starting_station.city)
     expect(page).to have_content(Trip.most_ending_station.name)
@@ -25,5 +25,24 @@ describe "user visits trip dashboard" do
     expect(page).to have_content(Trip.least_popular_date)
     expect(page).to have_content("rides in")
     expect(page).to have_content("of all subscriptions")
+  end
+  it "displays weather stats for longest and shortest rides" do
+    user = User.new(username: "Guy Fieri", password: "donkey$auce")
+    create(:station)
+    create(:station)
+    create_list(:trip, 20)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit trips_dashboard_path
+    expect(page).to have_content("Weather during the longest ride:")
+    expect(page).to have_content(Trip.longest_ride.condition.mean_temp_f)
+    expect(page).to have_content(Trip.longest_ride.condition.mean_wind_speed)
+    expect(page).to have_content(Trip.longest_ride.condition.mean_humidity)
+    expect(page).to have_content(Trip.longest_ride.condition.cloud_cover)
+    expect(page).to have_content("Weather during the shortest ride:")
+    expect(page).to have_content(Trip.shortest_ride.condition.mean_temp_f)
+    expect(page).to have_content(Trip.shortest_ride.condition.mean_wind_speed)
+    expect(page).to have_content(Trip.shortest_ride.condition.mean_humidity)
+    expect(page).to have_content(Trip.shortest_ride.condition.cloud_cover)
   end
 end
