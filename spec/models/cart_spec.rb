7e7b163773bd7d043c0778852567560f965c74c3
amj133 <amj@vt.edu>
2018-02-21 17:02:21 -0700

    cart spec includes instance methods #add_accessory and #count_of - test passing

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -7,5 +7,21 @@ describe Cart, type: :model do
 
       expect(cart.total_count).to eq(4)
     end
+
+    it "#add_accessory changes content count" do
+      cart = Cart.new({"1" => 1, "2" => 3})
+
+      cart.add_accessory(1)
+      cart.add_accessory(2)
+
+      expect(cart.contents).to eq({"1" => 2, "2" => 4})
+    end
+
+    it "#count_of returns count of specific accessory" do
+      cart = Cart.new({"1" => 1, "2" => 3})
+
+      expect(cart.count_of(1)).to eq(1)
+      expect(cart.count_of(2)).to eq(3)
+    end
   end
 end
