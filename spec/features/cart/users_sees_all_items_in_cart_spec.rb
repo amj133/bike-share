a84f1312f507694d2ad68b7410a9c758bbfb13ed
amj133 <amj@vt.edu>
2018-02-22 20:44:37 -0700

    add after clicking checkout displays message and directs to user dashboard written and passing - orders not yet created

--- spec/features/cart/users_sees_all_items_in_cart_spec.rb
+++ spec/features/cart/users_sees_all_items_in_cart_spec.rb
@@ -25,7 +25,7 @@ describe "user can view all items in cart" do
       expect(page).to have_content("Quantity: 1")
       expect(page).to have_content("Subtotal: 225")
       expect(page).to have_content("Cart Total: 335")
-      expect(page).to have_button("Checkout")
+      expect(page).to have_link("Checkout")
     end
 
     it "after clicking checkout, displays message and directs to user dashboard" do
@@ -33,7 +33,11 @@ describe "user can view all items in cart" do
       accessory_2 = create(:accessory, price: 225)
       bob = User.create(username: "bob", password: "test", email:"bob@gmail.com")
 
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(bob)
+      visit root_path
+      click_link("Login")
+      fill_in("Username", with: "bob")
+      fill_in("Password", with: "test")
+      click_button("Log in")
 
       visit bike_shop_path
       click_on("Add to cart", match: :first)
@@ -41,8 +45,8 @@ describe "user can view all items in cart" do
       visit cart_path
       click_on("Checkout")
 
-      expect(current_path).to eq(user_dashboard_path(bob))
-      expect(page).to have_content("You have successfully submitted your oder.")
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content("You have successfully submitted your order.")
     end
   end
 end
