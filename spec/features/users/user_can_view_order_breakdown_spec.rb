7619e8a066353fea3ddc103b252490d09ec1e383
amj133 <amj@vt.edu>
2018-02-23 14:56:48 -0700

    write user can view order breakdown spec

---
+++ spec/features/users/user_can_view_order_breakdown_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe "user can view order breakdown" do
+  context "visits dashboard and clicks on specific order" do
+    it "shows order break down" do
+      accessory_1 = create(:accessory, price: 110)
+      accessory_2 = create(:accessory, price: 225)
+      bob = User.create(username: "bob", password: "test", email:"bob@gmail.com")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(bob)
+
+      visit bike_shop_path
+      click_on("Add to cart", match: :first)
+      all('#add-to-cart')[1].click
+      click_on("Checkout")
+      click_on("Order 1")
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content(accessory_1.name)
+      expect(page).to have_content("Quantity: 1")
+      expect(page).to have_content("Subtotal: 110")
+      expect(page).to have_content(accessory_2.name)
+      expect(page).to have_content("Subtotal: 225")
+      expect(page).to have_content("Order Total: 335")
+      expect(page).to have_content("Status: Ordered")
+      expect(page).to have_content("Submitted: #{DateTime.now.strftime("%l:%M %P, %-m/%-d/%Y")}")
+    end
+  end
+end
