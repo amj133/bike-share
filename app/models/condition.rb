3068d5a0eb3dae20cce31efd7915bd34505f9338
Andrew Dymond <andy.dymond@gmail.com>
2018-02-22 20:45:32 -0700

    Format trips index

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -2,4 +2,8 @@ class Condition < ApplicationRecord
   validates_presence_of :date, :max_temp_f, :mean_temp_f, :min_temp_f, :mean_humidity, :mean_visibility_miles, :mean_wind_speed, :precipitation_inches
 
   has_many :trips, class_name: 'Trip', foreign_key: 'start_date', primary_key: 'date'
+
+  def readable_date
+    date.strftime("%b %d, %Y")
+  end
 end
