189fbaf8a30e78b09ecdde0a3346bfa26b9acd15
Andrew Dymond <andy.dymond@gmail.com>
2018-02-24 15:33:24 -0700

    Add #rides_by_date test to condition model spec

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -17,13 +17,20 @@ describe Condition, type: :model do
   end
 
   describe "class methods" do
-    xit "#group_by_10_degree_intervals" do
-      create(:station, id: 1)
-      create(:station, id: 2)
-      create_list(:condition, 15, max_temp_f: 8)
-      create_list(:trip, 8, start_station_id: 1, end_station_id: 2)
-      
-      expect(Condition.group_by_10_degree_intervals).to eq()
+    describe "class methods" do
+      it "#rides_by_date" do
+        date = Time.new(2001,2,3)
+        date_2 = Time.new(2009,2,3)
+        create(:station, id: 1)
+        create(:station, id: 2)
+        create_list(:condition, 10, max_temp_f: 8, date: date)
+        create_list(:condition, 9, max_temp_f: 4, date: date_2)
+        create_list(:trip, 8, start_station_id: 1, end_station_id: 2, start_date: date)
+        create_list(:trip, 8, start_station_id: 1, end_station_id: 2, start_date: date_2)
+
+        expect(Condition.rides_by_date("max_temp_f", 7, 9)).to eq({date => 80})
+        expect(Condition.rides_by_date("max_temp_f", 3, 9)).to eq({date => 80, date_2 => 72})
+      end
     end
   end
 end
