f513a792f4c2bda8a20020d5c444a6abd31318d4
amj133 <amj@vt.edu>
2018-02-24 11:05:50 -0700

    add class method test #by_status to order model spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -11,4 +11,29 @@ describe Order, type: :model do
     it {should belong_to(:user)}
     it {should have_many(:accessories).through(:order_accessories)}
   end
+
+  describe "class methods" do
+    it "#by_status returns all orders for given status" do
+      bob = User.create(username: "bob",
+                        password: "test",
+                        email:"bob@gmail.com",
+      order_1 = bob.orders.create(status: "Ordered",
+                                  total: 1111,
+                                  submitted: DateTime.now)
+      order_2 = bob.orders.create(status: "Ordered",
+                                  total: 2222,
+                                  submitted: DateTime.now)
+      order_3 = bob.orders.create(status: "Paid",
+                                  total: 3333,
+                                  submitted: DateTime.now)
+      order_4 = bob.orders.create(status: "Cancelled",
+                                  total: 44444,
+                                  submitted: DateTime.now)
+      order_5 = bob.orders.create(status: "Completed",
+                                  total: 5555,
+                                  submitted: DateTime.now)
+
+      expect(Order.by_status("Ordered")).to eq([order_1, order_2])
+    end
+  end
 end
