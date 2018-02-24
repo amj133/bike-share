693364a2d025d9f20e3d45897d90117521bd9ad0
Andrew Dymond <andy.dymond@gmail.com>
2018-02-24 15:48:37 -0700

    Add #average_rides_per_day test to condition model spec

--- spec/models/condition_spec.rb
+++ spec/models/condition_spec.rb
@@ -17,20 +17,31 @@ describe Condition, type: :model do
   end
 
   describe "class methods" do
-    describe "class methods" do
-      it "#rides_by_date" do
-        date = Time.new(2001,2,3)
-        date_2 = Time.new(2009,2,3)
-        create(:station, id: 1)
-        create(:station, id: 2)
-        create_list(:condition, 10, max_temp_f: 8, date: date)
-        create_list(:condition, 9, max_temp_f: 4, date: date_2)
-        create_list(:trip, 8, start_station_id: 1, end_station_id: 2, start_date: date)
-        create_list(:trip, 8, start_station_id: 1, end_station_id: 2, start_date: date_2)
+    it "#rides_by_date" do
+      date = Time.new(2001,2,3)
+      date_2 = Time.new(2009,2,3)
+      create(:station, id: 1)
+      create(:station, id: 2)
+      create(:condition, max_temp_f: 8, date: date)
+      create(:condition, max_temp_f: 4, date: date_2)
+      create_list(:trip, 8, start_station_id: 1, end_station_id: 2, start_date: date)
+      create_list(:trip, 10, start_station_id: 1, end_station_id: 2, start_date: date_2)
 
-        expect(Condition.rides_by_date("max_temp_f", 7, 9)).to eq({date => 80})
-        expect(Condition.rides_by_date("max_temp_f", 3, 9)).to eq({date => 80, date_2 => 72})
-      end
+      expect(Condition.rides_by_date("max_temp_f", 7, 9)).to eq({date => 8})
+      expect(Condition.rides_by_date("max_temp_f", 3, 9)).to eq({date => 8, date_2 => 10})
+    end
+
+    it "#average_rides_per_day" do
+      date = Time.new(2001,2,3)
+      date_2 = Time.new(2009,2,3)
+      create(:station, id: 1)
+      create(:station, id: 2)
+      create(:condition, max_temp_f: 8, date: date)
+      create(:condition, max_temp_f: 4, date: date_2)
+      create_list(:trip, 8, start_station_id: 1, end_station_id: 2, start_date: date)
+      create_list(:trip, 10, start_station_id: 1, end_station_id: 2, start_date: date_2)
+
+      expect(Condition.average_rides_per_day("max_temp_f", 3, 9)).to eq(9)
     end
   end
 end
