d7eec27b91f7afe3d4b60a7d8fc2aa4774cd054a
amj133 <amj@vt.edu>
2018-02-25 16:41:32 -0700

    create AddTimestampsToOrders migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180225181105) do
+ActiveRecord::Schema.define(version: 20180225233713) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,8 @@ ActiveRecord::Schema.define(version: 20180225181105) do
     t.string "status"
     t.datetime "submitted"
     t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
