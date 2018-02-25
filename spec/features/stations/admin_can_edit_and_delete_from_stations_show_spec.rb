ba90761140c457db5d7be89ba05278aabda3266c
Andrew Dymond <andy.dymond@gmail.com>
2018-02-24 21:16:29 -0700

    Edit indentation in admin_can_edit_and_delete_from_stations_show_spec

--- spec/features/stations/admin_can_edit_and_delete_from_stations_show_spec.rb
+++ spec/features/stations/admin_can_edit_and_delete_from_stations_show_spec.rb
@@ -3,10 +3,60 @@ require 'rails_helper'
 describe "Admin sees station show"  do
   it "Admin see edit button and delete button"  do
     station = create(:station, id: 1)
-      trip = create(:trip, start_station_id: 1, end_station_id: 1)
+    trip    = create(:trip, start_station_id: 1, end_station_id: 1)
+    user    = User.create(username: "Kyle", email: "email", password: "password" ,role: 1)
 
-      user = User.create(username: "Kyle", email: "email",
-      password: "password" ,role: 1)
+    visit root_path
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit station_path(station.slug)
+
+    expect(page).to have_link("Edit")
+    expect(page).to have_link("Delete")
+  end
+end
+
+describe "user sees station show"  do
+  it "user doesn't see edit button and delete button"  do
+    station = create(:station, id: 1)
+    trip = create(:trip, start_station_id: 1, end_station_id: 1)
+    user = User.create(username: "Kyle", email: "email", password: "password" )
+
+    visit root_path
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit station_path(station.slug)
+
+    expect(page).to_not have_link("Edit")
+    expect(page).to_not have_link("Delete")
+  end
+end
+
+describe "Admin sees station show"  do
+  it "Admin is able to delete station"  do
+    station = create(:station, id: 1)
+    trip = create(:trip, start_station_id: 1, end_station_id: 1)
+    user = User.create(username: "Kyle", email: "email", password: "password" ,role: 1 )
+
+    visit root_path
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit station_path(station.slug)
+
+    click_link("Delete")
+    expect(page).to_not have_link("Delete")
+    expect(page).to have_content("Station #{station.name} deleted.")
+  end
+end
+
+  describe "Admin sees station show"  do
+    it "Admin is able to edit station"  do
+      station = create(:station, id: 1)
+      trip = create(:trip, start_station_id: 1, end_station_id: 1)
+      user = User.create(username: "Kyle", email: "email", password: "password" ,role: 1 )
 
       visit root_path
 
@@ -14,74 +64,13 @@ describe "Admin sees station show"  do
 
       visit station_path(station.slug)
 
-      expect(page).to have_link("Edit")
-      expect(page).to have_link("Delete")
+      click_link("Edit")
+
+      expect(page).to have_content("Edit Stations")
+      fill_in("Name", with: "big fun")
+
+      click_button("Update Station")
+
+      expect(page).to have_content("Station at big-fun")
     end
   end
-
-  describe "user sees station show"  do
-    it "user doesn't see edit button and delete button"  do
-      station = create(:station, id: 1)
-        trip = create(:trip, start_station_id: 1, end_station_id: 1)
-
-        user = User.create(username: "Kyle", email: "email",
-        password: "password" )
-
-        visit root_path
-
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-        visit station_path(station.slug)
-
-        expect(page).to_not have_link("Edit")
-        expect(page).to_not have_link("Delete")
-      end
-    end
-
-    describe "Admin sees station show"  do
-      it "Admin is able to delete station"  do
-        station = create(:station, id: 1)
-          trip = create(:trip, start_station_id: 1, end_station_id: 1)
-
-          user = User.create(username: "Kyle", email: "email",
-          password: "password" ,role: 1 )
-
-          visit root_path
-
-          allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-          visit station_path(station.slug)
-
-
-          click_link("Delete")
-          expect(page).to_not have_link("Delete")
-          expect(page).to have_content("Station Name3 deleted.")
-        end
-      end
-
-      describe "Admin sees station show"  do
-        it "Admin is able to edit station"  do
-          station = create(:station, id: 1)
-            trip = create(:trip, start_station_id: 1, end_station_id: 1)
-
-            user = User.create(username: "Kyle", email: "email",
-            password: "password" ,role: 1 )
-
-            visit root_path
-
-            allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-            visit station_path(station.slug)
-
-
-            click_link("Edit")
-
-            expect(page).to have_content("Edit Stations")
-            fill_in("Name", with: "big fun")
-            
-            click_button("Update Station")
-
-            expect(page).to have_content("Station at big-fun")
-
-        end
-      end
