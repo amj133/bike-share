ff70e4e027cadd4fcc9ac6a493d12bac2612ca9b
amj133 <amj@vt.edu>
2018-02-25 09:00:42 -0700

    added uniqueness to accessory name, updated model test, added cannot create accessory with duplicate name or without attributes to admin can create new accessory spec

--- spec/features/accessories/admin_can_create_accessory_spec.rb
+++ spec/features/accessories/admin_can_create_accessory_spec.rb
@@ -21,5 +21,57 @@ describe "admin can create an accessory" do
       expect(page).to have_content(Accessory.last.description)
       expect(page).to have_content(Accessory.last.price)
     end
+
+    it "cannot create accessory with duplicate name" do
+      bob = User.create(username: "bob",
+                        password: "test",
+                        email:"bob@gmail.com",
+                        role: 1)
+      create(:accessory, name: "helmet")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(bob)
+
+      visit bike_shop_new_path
+      fill_in('Name', with: 'helmet')
+      fill_in('Description', with: 'Keep your hands warm!')
+      fill_in('Price', with: 1234)
+      click_on('Create Accessory')
+
+      expect(current_path).to eq(accessories_path)
+      expect(Accessory.count).to eq(1)
+    end
+
+    it "cannot create accessory without name, descr, or price" do
+      bob = User.create(username: "bob",
+                        password: "test",
+                        email:"bob@gmail.com",
+                        role: 1)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(bob)
+
+      visit bike_shop_new_path
+      fill_in('Description', with: 'Keep your hands warm!')
+      fill_in('Price', with: 1234)
+      click_on('Create Accessory')
+
+      expect(current_path).to eq(accessories_path)
+      expect(Accessory.count).to eq(0)
+
+      fill_in('Name', with: 'helmet')
+      fill_in('Description', with: '')
+      fill_in('Price', with: 1234)
+      click_on('Create Accessory')
+
+      expect(current_path).to eq(accessories_path)
+      expect(Accessory.count).to eq(0)
+
+      fill_in('Name', with: 'helmet')
+      fill_in('Description', with: 'Keep your hands warm!')
+      fill_in('Price', with: '')
+      click_on('Create Accessory')
+
+      expect(current_path).to eq(accessories_path)
+      expect(Accessory.count).to eq(0)
+    end
   end
 end
