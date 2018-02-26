8a4e788543306a80a8d3c87fe4f15fe4c98c1260
kylesallette <kylesallette@gmail.com>
2018-02-26 12:37:38 -0700

    fixing tests

--- spec/features/users/visitor_is_able_to_create_a_account_spec.rb
+++ spec/features/users/visitor_is_able_to_create_a_account_spec.rb
@@ -3,26 +3,29 @@ require 'rails_helper'
 describe "visitor is able to create a account" do
   describe "visits root path" do
     it "they answer info and they are logged in" do
+
        visit root_path
 
        click_on("Login")
 
        expect(current_path).to eq(login_path)
 
+      
        click_on("Create Account")
-       fill_in("Username", with: "bob")
+       fill_in("Username", with: "pal")
        fill_in("Password", with: "password")
-       fill_in("Email", with: "email")
+       fill_in("Email", with: "email@gmail.com")
+
        click_on("Submit")
 
-       expect(current_path).to eq("/dashboard")
+       expect(current_path).to eq(dashboard_path)
        expect(page).to have_content("Welcome #{User.last.username}")
        expect(page).to have_content("Logged in as #{User.last.username}")
        expect(page).to have_link("Logout")
     end
 
     it "User uses existing username and is redirected to new path" do
-      user = User.create!(username: "bob", password: "password", email: "email")
+      user = User.create!(username: "bob", password: "password", email: "email@gmail.com")
 
       visit root_path
       click_on("Login")
@@ -32,7 +35,7 @@ describe "visitor is able to create a account" do
       click_on("Create Account")
       fill_in("Username", with: "bob")
       fill_in("Password", with: "password")
-      fill_in("Email", with: "email")
+      fill_in("Email", with: "email@gmail.com")
       click_on("Submit")
 
       expect(page).to have_content("#{User.last.username} already exists")
