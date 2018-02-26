6adae9a9d7c6186483dadacbb98ab10b945f289d
kylesallette <kylesallette@gmail.com>
2018-02-26 12:29:55 -0700

    adding validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
-
+  validates_email_format_of :email, :message => 'is not looking good'
   validates_presence_of :username, :password
   validates_uniqueness_of :username
 
