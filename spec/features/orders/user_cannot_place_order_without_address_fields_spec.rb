297226e63113c7c2d2298448dc21b5bb6b2b8393
Andrew Dymond <andy.dymond@gmail.com>
2018-02-27 15:22:12 -0700

    Add user_cannot_place_order_without_address_fields_spec.rb

---
+++ spec/features/orders/user_cannot_place_order_without_address_fields_spec.rb
@@ -0,0 +1,22 @@
+require "rails_helper"
+
+describe "user tries to place order without shipping information" do
+  it "redirects them to update their info with shipping info" do
+    user = User.create(username: "Bob", password: "password")
+    create(:accessory)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit login_path
+    fill_in "Username", with: user.username
+    fill_in "Password", with: user.password
+    click_on "Log in"
+
+    visit bike_shop_path
+    click_on "Add to cart"
+    visit cart_path
+    click_on "Checkout"
+
+    expect(current_path).to eq(edit_user_path(user))
+    expect(page).to have_content("You must have all shipping information to checkout!")
+  end
+end
