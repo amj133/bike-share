f95f5a11076cd69acf274058ea4c94d365468db4
Andrew Dymond <andy.dymond@gmail.com>
2018-02-26 18:24:09 -0700

    Add abc scope to accessory model, change method call in accessories controllers

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,8 +1,7 @@
 class Accessory < ApplicationRecord
   validates_presence_of :name, :description, :price
   validates_uniqueness_of :name
-
   has_many :order_accessories
   has_many :orders, through: :order_accessories
-
+  scope :abc, -> {order('lower(name)')}
 end
