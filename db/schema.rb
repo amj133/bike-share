cd082cebeb2c0f304ff4707b491d5a3feb0b968b
Andrew Dymond <andy.dymond@gmail.com>
2018-02-26 18:29:59 -0700

    Add columns for first_name, last_name, street, city, state, and zipcode to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226200846) do
+ActiveRecord::Schema.define(version: 20180227012846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,6 +104,12 @@ ActiveRecord::Schema.define(version: 20180226200846) do
     t.string "email"
     t.string "password_digest"
     t.integer "role", default: 0
+    t.string "first_name"
+    t.string "last_name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.integer "zipcode"
   end
 
   add_foreign_key "order_accessories", "accessories"
