17c108632b5826641f4e96df275c9cdd5e76eee5
amj133 <amj@vt.edu>
2018-02-26 18:47:09 -0700

    admin can retire or activate accessories from admin bike shop path

--- app/controllers/admin/accessories_controller.rb
+++ app/controllers/admin/accessories_controller.rb
@@ -15,6 +15,20 @@ class Admin::AccessoriesController < Admin::BaseController
     redirect_to admin_bike_shop_path
   end
 
+  def retire
+    accessory = Accessory.find(params[:accessory_id])
+    accessory.update(status: "inactive")
+
+    redirect_to admin_bike_shop_path
+  end
+
+  def activate
+    accessory = Accessory.find(params[:accessory_id])
+    accessory.update(status: "active")
+
+    redirect_to admin_bike_shop_path
+  end
+
   private
 
   def accessory_params
